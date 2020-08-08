d37cdbb979168887c58af53584d24ab107cc04af
shourovrahman <shourovju43@gmail.com>
2020-08-08 23:23:19 +0600

    Added a new file name Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public static void main(string[] args) {
+        System.out.println("Vs Code is Awesome...");
+        
+        greeting("Shourov Rahman");
+    }
+    public static void greeting(String name) {
+        System.out.println("Welcome" + name);
+    }
+}
